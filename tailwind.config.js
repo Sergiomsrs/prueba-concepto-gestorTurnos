bab27a88e1c415c76f5b790473b7c91737bdb5a4
Sergio <sergiomsrs@gmail.com>
2024-09-13 16:40:44 +0200

    Refactorizar adaptación a la API, actualizar URL de consulta y añadir estilos de borde y altura personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        'half-height': '2px', // Asegúrate de que el grosor sea el mismo
+      },
+      height: {
+        'half': '50%',
+      },
+    },
   },
   plugins: [
-    //require('@tailwindcss/forms'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.half-height-border::after': {
+          content: '""',
+          position: 'absolute',
+          top: '50%',
+          right: '0',
+          height: '50%',
+          width: '1px', // Asegúrate de que el grosor sea el mismo
+          backgroundColor: 'grey',
+        },
+      });
+    },
   ],
-}
+};
