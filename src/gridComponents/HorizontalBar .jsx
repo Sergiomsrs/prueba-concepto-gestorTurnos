91ed6e13df115cc01094ced5050a50b5c7445260
Sergio <sergiomsrs@gmail.com>
2024-11-15 10:35:45 +0100

    bug mousedown corregido

--- src/gridComponents/HorizontalBar .jsx
+++ src/gridComponents/HorizontalBar .jsx
@@ -16,7 +16,7 @@ export const HorizontalBar = ({ username, hours, onHourChange, teamWork, shiftDu
     if (isSelecting && startSelection !== null) {
       const selectionStart = Math.min(startSelection, index);
       const selectionEnd = Math.max(startSelection, index);
-      const isStartSelected = hours[startSelection] !== "Null";
+      const isStartSelected = hours[startSelection] !== "Null" || hours[startSelection] === "PTO";
 
       for (let i = selectionStart; i <= selectionEnd; i++) {
         if (isInputDisabled(i)) {
@@ -47,18 +47,18 @@ export const HorizontalBar = ({ username, hours, onHourChange, teamWork, shiftDu
         <td
           className={`relative md:w-24 w-12 text-center align-bottom ${index % 4 === 3 ? 'border-45-height' : ''}`}
           key={index}
-          onMouseDown={() => handleMouseDown(index)}
-          onMouseEnter={() => handleMouseEnter(index)}
-          onMouseUp={handleMouseUp}
         >
           <input
             type="checkbox"
             className={`w-4 h-4 p-0 m-0 appearance-none border border-gray-400 rounded-3xl ${isInputDisabled(index) || value === "PTO" ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'
-              } ${value !== "Null" && value !== "NP" ? 'bg-indigo-500' : 'bg-neutral-200'
+              } ${value !== "Null" && value !== "PTO" ? 'bg-indigo-500' : 'bg-neutral-200'
               } relative`}
-            checked={value !== "Null" && value !== "NP"}
+            checked={value !== "Null" && value !== "PTO"}
             onChange={(event) => handleClick(index, event.target.checked)}
             disabled={isInputDisabled(index) || value === "PTO"}
+            onMouseDown={() => handleMouseDown(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseUp={handleMouseUp}
           />
           {index % 4 === 3 && (
             <div className="absolute right-0 top-1/2 h-1/2 bg-slate-400" style={{ zIndex: -1, width: 1 }}></div>
