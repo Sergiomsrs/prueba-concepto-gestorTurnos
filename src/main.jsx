4d4271bf13f8e6efb7fb02eb603d99b0f8991bf0
Sergio <sergiomsrs@gmail.com>
2025-07-24 18:36:59 +0200

    feat: add deployment scripts and update routing
    - Added "predeploy" and "deploy" scripts to package.json for building and deploying the app using gh-pages.
    - Switched from BrowserRouter to HashRouter in main.jsx for better compatibility with GitHub Pages.
    - Set base path in vite.config.js to '/prueba-concepto-gestorTurnos/' for proper routing.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { MainPage } from './pages/MainPage'
 import { AppProvider } from './context/AppProvider'
 import { AuthProvider } from './timeTrack/context/AuthContext'
@@ -10,12 +10,12 @@ import { AuthProvider } from './timeTrack/context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppProvider>
     <AuthProvider>
         <MainPage />
     </AuthProvider>
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
