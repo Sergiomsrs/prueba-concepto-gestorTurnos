bd8bdfe92f043eaa5768c1de7dae0db100a912f5
Sergio <sergiomsrs@gmail.com>
2025-07-24 19:57:55 +0200

    Implement code changes to enhance functionality and improve performance

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -3,7 +3,8 @@ export const LoginForm = () => {
   return (
 
     <section className="w-3/4 bg-gray-50 dark:bg-gray-900 ">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  bg-[url('src/images/bg-image-loginForm.webp')] bg-cover bg-center bg-no-repeat">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto bg-[url('/bg-image-loginForm.webp')] bg-cover bg-center bg-no-repeat">
+
       
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
