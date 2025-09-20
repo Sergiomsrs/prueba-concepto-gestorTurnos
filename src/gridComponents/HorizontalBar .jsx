3b1cf39c7028a1935a7ebe657c1c639b81dfc582
Sergio <sergiomsrs@gmail.com>
2025-09-20 11:57:59 +0200

    fix: corregir nombre de función para abrir modal en HorizontalBar

--- src/gridComponents/HorizontalBar .jsx
+++ src/gridComponents/HorizontalBar .jsx
@@ -9,7 +9,7 @@ export const HorizontalBar = ({
   setStartSelection, handleMouseUp, id, rowIndex, inputRefsMatrix, numRows, numCols
 }) => {
 
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [lastFocusedIndex, setLastFocusedIndex] = useState(null);
   const inputRefs = React.useRef([]);
 
