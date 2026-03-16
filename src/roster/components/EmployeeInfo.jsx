75e6bd1be53f055bd4adcd2dcdad2510f0e73331
Sergio <sergiomsrs@gmail.com>
2026-03-16 08:34:58 +0100

    structure: eliminados archivos de roster

--- src/roster/components/EmployeeInfo.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from 'react';
-
-export const EmployeeInfo = memo(({ name, teamWork }) => (
-    <div className="flex gap-3 min-w-[200px]">
-        <p className="font-medium">{name}</p>
-        <p className="text-gray-600">{teamWork}</p>
-    </div>
-));
